5727344e1553d3394a9a655f96c612c8655622e8
Lucas Montanari <Lmonty2011@gmail.com>
2021-06-25 01:30:40 -0700

    added basic routes to start

--- BreweryBE/config/routes.rb
+++ BreweryBE/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :beer, only: [:index, :show]
+  resources :brewery, only: [:index, :show]
+  resources :member, only: [:index, :show]
 end
