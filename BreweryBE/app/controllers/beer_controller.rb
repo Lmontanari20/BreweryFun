3d92d317015931b275e0b281a417d80241e9bfc9
Lucas Montanari <Lmonty2011@gmail.com>
2021-06-26 13:57:40 -0700

    added edit/update as well as a partial for new and edit

--- BreweryBE/app/controllers/beer_controller.rb
+++ BreweryBE/app/controllers/beer_controller.rb
@@ -10,10 +10,22 @@ class BeerController < ApplicationController
     def new 
         @beer = Beer.new
         @breweries = Brewery.all
+        @action = "new"
     end
 
     def create 
         @beer = Beer.create(name: params[:beer][:name], typeBeer: params[:beer][:typeBeer], alc: params[:beer][:alc], brewery_id: params[:beer][:brewery_id])
+        redirect_to @beer
+    end
+
+    def edit 
+        @beer = Beer.find(params[:id])
+        @breweries = Brewery.all
+    end
+
+    def update 
+        @beer = Beer.find(params[:id])
+        @beer.update(name: params[:beer][:name], typeBeer: params[:beer][:typeBeer], alc: params[:beer][:alc], brewery_id: params[:beer][:brewery_id])
         redirect_to @beer
     end
 end
--- BreweryBE/config/routes.rb
+++ BreweryBE/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :beer, only: [:index, :show, :new, :create], as: :beers
+  resources :beer, only: [:index, :show, :new, :create, :edit, :update], as: :beers
   resources :brewery, only: [:index, :show]
   resources :member, only: [:index, :show]
 end
